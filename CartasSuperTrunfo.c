f31dc3ac3ac6c5b4aa4f616a28bdbec19a719de0
Getúlio <rodolfobraga048@gmail.com>
2026-05-08 20:21:21 +0000

    Adicionei outra mensagem

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   printf("Olá Mundo!");
   // Área para entrada de dados
-
+  printf("Olá pessoas");
   // Área para exibição dos dados da cidade
 
 return 0;
